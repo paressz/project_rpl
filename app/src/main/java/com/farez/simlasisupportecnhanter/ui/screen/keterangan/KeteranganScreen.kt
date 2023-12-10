0fa7b93c581ae3e7cc4666c9b79cdd98e9cd30eb
paress <farez0830@gmail.com>
2023-12-10 16:42:10 +0700

    + padding for rule screen

--- app/src/main/java/com/farez/simlasisupportecnhanter/ui/screen/keterangan/KeteranganScreen.kt
+++ app/src/main/java/com/farez/simlasisupportecnhanter/ui/screen/keterangan/KeteranganScreen.kt
@@ -3,12 +3,14 @@ package com.farez.simlasisupportecnhanter.ui.screen.keterangan
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.farez.simlasisupportecnhanter.R
 
 @Composable
@@ -17,7 +19,8 @@ fun KeteranganScreen() {
         modifier = Modifier
             .fillMaxSize()
             .navigationBarsPadding()
-            .statusBarsPadding(),
+            .statusBarsPadding()
+            .padding(horizontal = 16.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
